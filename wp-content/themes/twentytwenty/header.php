e54bc36271e009b6e43cc3cea4377983ebc6e9f5
Hazar Nazari <hazar.nazari@kuehne-nagel.com>
2022-09-04 13:27:48 +0200

    test changes

--- wp-content/themes/twentytwenty/header.php
+++ wp-content/themes/twentytwenty/header.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * Header file for the Twenty Twenty WordPress default theme.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package WordPress
- * @subpackage Twenty_Twenty
- * @since Twenty Twenty 1.0
- */
-
+/*
+	nazaricode.com
+*/
 ?><!DOCTYPE html>
 
 <html class="no-js" <?php language_attributes(); ?>>
@@ -16,8 +9,6 @@
 	<head>
 
 		<meta charset="<?php bloginfo( 'charset' ); ?>">
-		
-		
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 
 		<!-- adsense -->        
